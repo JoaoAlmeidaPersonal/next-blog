ad08dca5725eeddb9c137842040990542fd27e3c
DESKTOP-TBTGG3E\ZXCV <jtiago360@gmail.com>
2023-03-16 23:50:57 +0000

    Added reading the markdown into posts in a better way!

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,22 +1,53 @@
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { Prism } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-const DUMMY_POST = {
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  slug: "getting-started-with-nextjs",
-  content: "# This is a first post",
-};
+function PostContent(props) {
+  const imagePath = `/images/posts/${props.post.slug}/${props.post.image}`;
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${props.post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+      code(code) {
+          const _array = code.className.split("-");
+          const _language = _array[1];
+
+          return (
+              <Prism language={_language} style={atomDark}>
+                  {code.children[0]}
+              </Prism>
+          );
+      },
+  };
 
   return (
     <article className={classes.content}>
-      <PostHeader image={imagePath} title={DUMMY_POST.title} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader image={imagePath} title={props.post.title} />
+      <ReactMarkdown components={customRenderers}>
+        {props.post.content}
+      </ReactMarkdown>
     </article>
   );
 }
