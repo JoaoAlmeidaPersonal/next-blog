e1c380a9987e818af2dceba354a368e0e735efc2
João Almeida <jtiago360@gmail.com>
2023-03-21 02:01:54 +0000

    Added some fixes and meta tags!

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import {Fragment} from "react";
 import Hero from "../components/homepage/hero";
 import FeaturedPosts from "../components/homepage/featured-posts";
 import {getFeaturedPosts} from "../lib/posts-util";
+import Head from "next/head";
 
 function HomePage(props) {
     return <Fragment>
+        <Head>
+            <title>Welcome to my blog!</title>
+            <meta name='description' content='I post about web development!'/>
+        </Head>
             <Hero />
         <FeaturedPosts posts={props.posts}/>
         </Fragment>;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,18 @@
 import AllPosts from "../../components/posts/all-posts";
 import {getAllPosts, getFeaturedPosts} from "../../lib/posts-util";
+import {Fragment} from "react";
+import Head from "next/head";
+
 
 function AllPostsPage(props) {
-    return <AllPosts posts={props.posts}/>
+    return <Fragment>
+        <Head>
+            <title>All My Posts!</title>
+            <meta name='description' content='A list of all my posts!'/>
+        </Head>
+        <AllPosts posts={props.posts}/>
+
+    </Fragment>
 }
 
 export default AllPostsPage;
